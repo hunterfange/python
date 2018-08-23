3f53f26810e9bf81da0009157ed739f8e04e4eff
hunterfange <34684544+hunterfange@users.noreply.github.com>
2018-08-23 11:01:52 +0800

    test

---
+++ 第10章文件和异常.py
@@ -0,0 +1,17 @@
+#第10章 文件异常
+'''
+import json
+
+
+
+filename='number.json'
+with open(filename,)as f_obj:
+    numbers=json.load(f_obj)
+
+print(numbers)
+'''
+import matplotlib.pyplot as plt
+
+square=[1,4,9,16,25]
+plt.plot(square)
+plt.show()
